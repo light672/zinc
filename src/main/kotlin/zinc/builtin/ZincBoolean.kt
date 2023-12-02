c9bde6ce9632c33d6003b39c818d537dfc20250b
light672 <lightdevelopment6@gmail.com>
2023-12-02 15:45:38 -0500

    Shorten ZincBoolean class into one line.

--- src/main/kotlin/zinc/builtin/ZincBoolean.kt
+++ src/main/kotlin/zinc/builtin/ZincBoolean.kt
@@ -1,5 +1,3 @@
 package zinc.builtin
 
-data class ZincBoolean(val value: Boolean) : ZincValue() {
-	override val truthy get() = value
-}
+data class ZincBoolean(override val truthy: Boolean) : ZincValue()
