06fbccca97ffbd9a022b6bc448f75cc0ede8cf23
light672 <lightdevelopment6@gmail.com>
2025-01-21 13:30:59 -0800

    Delete unused Either

--- src/main/kotlin/com/light672/zinc/Either.kt
+++
@@ -1,9 +0,0 @@
-package com.light672.zinc
-
-internal sealed interface Either<out A, out B> {
-	class Left<A>(val value: A) : Either<A, Nothing>
-	class Right<B>(val value: B) : Either<Nothing, B>
-	companion object {
-		fun <A, B> or(a: A?, b: B) = a?.let { Left(a) } ?: Right(b)
-	}
-}
